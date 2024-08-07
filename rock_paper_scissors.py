c720eadcf4bbfd9f65f3be06776cacf27d98911b
evilgelato <jonathonjollota@gmail.com>
2024-08-06 18:06:00 -0700

    1

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,21 +1,56 @@
 
-
-user_input1 = input('What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.')
-
+import math 
 
 
 
+#logic for selection of rock paper scissors and output of the user selection
+
+#if logic
+#user_input1 = input('What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.')
+# if int(user_input1) == 0:
+#     print('You have selected "Rock".')
+# elif int(user_input1) == 1:
+#     print('You have selected "Paper".')
+# elif int(user_input1) == 2:
+#     print('You have selected "Scissors".')
+# else:
+#     print('Please select a valid number next time.')
+#     user_input1 = input('What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.')
+
+#while logic
+count  = 0
+while count == 0:
+    user_input1 = input('What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.')
+    if int(user_input1) == 0:
+        print('You have selected "Rock".')
+        count += 1
+    elif int(user_input1) == 1:
+        print('You have selected "Paper".')
+        count += 1
+    elif int(user_input1) == 2:
+        print('You have selected "Scissors".')
+        count += 1
+    else:
+        print('Please select a valid number next time.')
+    
+
+
+
+#randomization of the choice of the computer
 
 
 
 
 
 
-if int(user_input1) == 0:
-    print('You have selected "Rock".')
-elif int(user_input1) == 1:
-    print('You have selected "Paper".')
-elif int(user_input1) == 2:
-    print('You have selected "Scissors".')
-else:
-    print('Please select a valid number next time.')
+
+#outcome of the match
+
+
+
+
+
+
+
+
+
