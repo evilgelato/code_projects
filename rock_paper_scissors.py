efbcaa204caf3ce5f8fcb02432fc8a00d0fec942
evilgelato <jonathonjollota@gmail.com>
2024-08-06 17:55:22 -0700

    updated

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,6 +5,9 @@ user_input1 = input('What do you choose? Type 0 for Rock, 1 for Paper, or 2 for 
 
 if int(user_input1) == 0:
     print('You have selected "Rock".')
-
-
-
+elif int(user_input1) == 1:
+    print('You have selected "Paper".')
+elif int(user_input1) == 2:
+    print('You have selected "Scissors".')
+else:
+    input('Please select a valid number. \n Type 0 for Rock, 1 for Paper, or 2 for Scissors.')
